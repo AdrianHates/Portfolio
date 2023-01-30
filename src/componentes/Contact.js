1d03d4b0e16dd61d93d9aa6bc138ef91b35486cb
AizenSousuke <kurono1807@gmail.com>
2023-01-30 04:23:11 -0500

    github

--- src/componentes/Contact.js
+++ src/componentes/Contact.js
@@ -22,7 +22,7 @@ const Contact = ( { id } ) => {
       <div>
       <Red src='https://upload.wikimedia.org/wikipedia/commons/e/ee/Logo_de_Facebook.png' href='https://www.facebook.com/HerlessOliverRamosEspinoza/'></Red>
       <Red src='https://www.xaviro.com/assets/e7a394e7937e0063e9d7.png' href='https://www.linkedin.com/in/herless-oliver-ramos-espinoza-08b237256/'></Red>
-      <Red src='https://www.seekpng.com/png/full/119-1191363_share-button-how-to-twitter-symbol-in-circle.png' href='https://twitter.com/IoriYag81098860'></Red>
+      <Red src='https://www.xaviro.com/assets/d5126c01a9950836e627.png' href='https://github.com/AdrianHates'></Red>
       <Red src='https://pbs.twimg.com/profile_images/1276770212927410176/qTgTIejk_400x400.jpg' href='https://www.freecodecamp.org/herlessoliver'></Red>
       </div>
   </div>
