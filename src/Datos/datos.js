f585e9cd2530da6973ec12120d55c9d64c41701c
AizenSousuke <kurono1807@gmail.com>
2024-05-18 00:36:22 -0500

    fix(datos): fixed url project

--- src/Datos/datos.js
+++ src/Datos/datos.js
@@ -1,91 +1,96 @@
-export const proyectos=[
+export const proyectos = [
   {
-    texto: 'Landing-page Web',
-    categoria: 'web',
-    src: 'https://i.imgur.com/R2Fdi1P.jpg',
-    href: 'https://adrianhates.github.io/landingpageweb/',
-    tecnology: ['html', 'css', 'javascript', 'react']
-
+    texto: "Changomon",
+    categoria: "game",
+    code: "https://github.com/AdrianHates/game-simon",
+    src: "https://i.imgur.com/b3eZ6cV.png",
+    href: "https://adrianhates.github.io/game-simon/",
+    tecnology: ["html", "css", "javascript", "react"],
   },
   {
-    texto: 'Template-home-adidas',
-    categoria: 'web',
-    src: 'https://i.imgur.com/QIJGBpI.jpg',
-    href: 'https://adrianhates.github.io/template-ecommerce/',
-
-    tecnology: ['html', 'css', 'javascript', 'react']
-
-
+    texto: "Pokemon Emerald",
+    categoria: "game",
+    code: "https://github.com/AdrianHates/pokemon",
+    src: "https://i.imgur.com/E9I0UCM.png",
+    href: "https://adrianhates.github.io/newpokemon",
+    tecnology: ["react", "css", "python", "postgresql", "typescript"],
   },
   {
-    texto: 'TiendaVirtualFull',
-    categoria: 'web',
-    src: 'https://i.imgur.com/g7yP0ue.jpg',
-    href: 'https://tiendavirtualfull.onrender.com/',
-    code: 'https://github.com/AdrianHates/tiendavirtualfull',
-    tecnology: ['html', 'css', 'javascript', 'react', 'node', 'mongo']
-  }, {
-    texto: 'Fastest Food',
-    categoria: 'web',
-    code: 'https://github.com/AdrianHates/fastestfood',
-    src: 'https://i.imgur.com/ckxyMmY.png',
-    href: 'https://adrianhates.github.io/fastestfood/',
-    tecnology: ['html', 'css', 'javascript', 'jquery'],
-  }, {
-      texto: 'Pokemon Emerald',
-      categoria: 'game',
-      code: 'https://github.com/AdrianHates/pokemon',
-      src: 'https://i.imgur.com/E9I0UCM.png',
-      href: 'https://adrianhates.github.io/newpokemon',
-      tecnology: ['react', 'css', 'python', 'postgresql', 'typescript']
-
-
-  }, {
-    texto: 'Changomon',
-    categoria: 'game',
-    code: 'https://github.com/AdrianHates/game-simon',
-    src: 'https://i.imgur.com/KshiWbY.png',
-    href: 'https://adrianhates.github.io/game-simon/',
-    tecnology: ['html', 'css', 'javascript', 'react']
-  }, {
-    texto: 'Game of Life',
-    categoria: 'app',
-    code: 'https://github.com/AdrianHates/gameoflife',
-    src: 'https://imgur.com/G0ebZTb.png', 
-    href: 'https://adrianhates.github.io/gameoflife/',
-    tecnology: ['html', 'css', 'javascript', 'react']
-  }, {
-    texto: 'Calculadora',
-    categoria: 'app',
-    src: 'https://i.imgur.com/aau1G4X.png', 
-    href: 'https://codepen.io/Fade-Out/full/PoaozoV',
-    tecnology: ['html', 'css', 'javascript', 'react']
-  }, {
-    texto: 'Tres Raya',
-    categoria: 'game',
-    code: 'https://github.com/AdrianHates/tresraya/tree/master',
-    src: 'https://i.imgur.com/f4iby7R.png', 
-    href: 'https://adrianhates.github.io/tresraya/',
-    tecnology: ['html', 'css', 'javascript', 'react']
-
-  }, {
-    texto: 'Tetris',
-    categoria: 'game',
-    code: 'https://github.com/AdrianHates/Tetris',
-    src: 'https://i.imgur.com/eIazWJO.png',
-    href: 'https://adrianhates.github.io/Tetris/',
-    tecnology: ['html', 'css', 'javascript', 'react']
+    texto: "Landing-page Web",
+    categoria: "web",
+    src: "https://i.imgur.com/R2Fdi1P.jpg",
+    href: "https://adrianhates.github.io/landingpageweb/",
+    tecnology: ["html", "css", "javascript", "react"],
   },
   {
-    texto: '25 + 5 Clock',
-    categoria: 'app',
-    src: 'https://i.imgur.com/mBLN4mR.png', 
-    href: 'https://codepen.io/Fade-Out/full/dyKGNJO',
-    tecnology: ['html', 'css', 'javascript', 'react']
-  }
-]
+    texto: "Template-home-adidas",
+    categoria: "web",
+    src: "https://i.imgur.com/QIJGBpI.jpg",
+    href: "https://adrianhates.github.io/template-ecommerce/",
+
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+  {
+    texto: "TiendaVirtualFull",
+    categoria: "web",
+    src: "https://i.imgur.com/g7yP0ue.jpg",
+    href: "https://tiendavirtualfull.onrender.com/",
+    code: "https://github.com/AdrianHates/tiendavirtualfull",
+    tecnology: ["html", "css", "javascript", "react", "node", "mongo"],
+  },
+  {
+    texto: "Fastest Food",
+    categoria: "web",
+    code: "https://github.com/AdrianHates/fastestfood",
+    src: "https://i.imgur.com/ckxyMmY.png",
+    href: "https://adrianhates.github.io/fastestfood/",
+    tecnology: ["html", "css", "javascript", "jquery"],
+  },
+  {
+    texto: "Game of Life",
+    categoria: "app",
+    code: "https://github.com/AdrianHates/gameoflife",
+    src: "https://imgur.com/G0ebZTb.png",
+    href: "https://adrianhates.github.io/gameoflife/",
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+  {
+    texto: "Calculadora",
+    categoria: "app",
+    src: "https://i.imgur.com/aau1G4X.png",
+    href: "https://codepen.io/Fade-Out/full/PoaozoV",
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+  {
+    texto: "Tres Raya",
+    categoria: "game",
+    code: "https://github.com/AdrianHates/tresraya/tree/master",
+    src: "https://i.imgur.com/f4iby7R.png",
+    href: "https://adrianhates.github.io/tresraya/",
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+  {
+    texto: "Tetris",
+    categoria: "game",
+    code: "https://github.com/AdrianHates/Tetris",
+    src: "https://i.imgur.com/eIazWJO.png",
+    href: "https://adrianhates.github.io/Tetris/",
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+  {
+    texto: "25 + 5 Clock",
+    categoria: "app",
+    src: "https://i.imgur.com/mBLN4mR.png",
+    href: "https://codepen.io/Fade-Out/full/dyKGNJO",
+    tecnology: ["html", "css", "javascript", "react"],
+  },
+];
 
 export const description = {
-  es: ["Soy una persona muy trabajadora y comprometida con tres meses de experiencia en prácticas en programación y ciencias exactas. Mi enfoque está en el desarrollo fullstack, destacando en JavaScript, React y Node.js. Durante mis prácticas, contribuí al desarrollo de proyectos web de alta calidad. Estoy comprometido con el aprendizaje continuo y emocionado por crecer en este campo."],
-  en: ["I am a very hard-working and committed person with three months of internship experience in programming and exact sciences. My focus is on fullstack development, highlighting JavaScript, React and Node.js. During my internship, I contributed to the development of high-quality web projects. I am committed to continuous learning and excited to grow in this field."]
-}
+  es: [
+    "Soy una persona muy trabajadora y comprometida con tres meses de experiencia en prácticas en programación y ciencias exactas. Mi enfoque está en el desarrollo fullstack, destacando en JavaScript, React y Node.js. Durante mis prácticas, contribuí al desarrollo de proyectos web de alta calidad. Estoy comprometido con el aprendizaje continuo y emocionado por crecer en este campo.",
+  ],
+  en: [
+    "I am a very hard-working and committed person with three months of internship experience in programming and exact sciences. My focus is on fullstack development, highlighting JavaScript, React and Node.js. During my internship, I contributed to the development of high-quality web projects. I am committed to continuous learning and excited to grow in this field.",
+  ],
+};
