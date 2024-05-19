b3a333eb149234967376bc4012a42ba793102e13
AizenSousuke <kurono1807@gmail.com>
2024-05-19 01:54:44 -0500

    fix project changomon url

--- src/Datos/datos.js
+++ src/Datos/datos.js
@@ -3,7 +3,7 @@ export const proyectos = [
     texto: "Changomon",
     categoria: "game",
     code: "https://github.com/AdrianHates/game-simon",
-    src: "https://i.imgur.com/zAaX0if.png",
+    src: "https://i.imgur.com/FtBUORc.png",
     href: "https://adrianhates.github.io/game-simon/",
     tecnology: ["html", "css", "javascript", "react"],
   },
